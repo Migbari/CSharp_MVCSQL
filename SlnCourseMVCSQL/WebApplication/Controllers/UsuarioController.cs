22c7666bda66c57c4d67609e746b1aeae5e43662
Migbari <webdevel.bin@gmail.com>
2020-05-24 17:11:21 -0300

    Commit Inicia

---
+++ SlnCourseMVCSQL/WebApplication/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using BDProjetoAplicacao;
+using BDProjetoDominio;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Inicial()
+        {
+            var appUsuario = new UsuarioAplicacao();
+            var listaUsuario = appUsuario.ListarTodos();
+            return View(listaUsuario);
+        }
+        [HttpPost]
+        public ActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                var appUsuario = new UsuarioAplicacao();
+                appUsuario.Salvar(usuario);
+                return RedirectToAction("Inicial");
+            }
+            return View(usuario);
+        }
+    }
+}
