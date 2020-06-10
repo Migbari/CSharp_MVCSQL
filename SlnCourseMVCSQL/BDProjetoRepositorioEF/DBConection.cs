2c45944b7e39f5338109a81745a90be2cb98133a
Migbari <webdevel.bin@gmail.com>
2020-06-09 23:47:18 -0300

    Criando Campos de Domínio

--- SlnCourseMVCSQL/BDProjetoRepositorioEF/DBConection.cs
+++ SlnCourseMVCSQL/BDProjetoRepositorioEF/DBConection.cs
@@ -1,5 +1,6 @@
 ﻿using BDProjetoDominio;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace BDProjetoRepositorioEF
 {
@@ -10,6 +11,13 @@ namespace BDProjetoRepositorioEF
 
         }
         public DbSet<Usuario> usuario { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Usuario>().Property(x => x.Nome).IsRequired().HasColumnType("varchar").HasMaxLength(45);
+            modelBuilder.Entity<Usuario>().Property(x => x.Cargo).IsRequired().HasColumnType("varchar").HasMaxLength(45);
+            modelBuilder.Entity<Usuario>().Property(x => x.Data).IsRequired().HasColumnType("date");
+        }   
     }
 }
 
---
+++ SlnCourseMVCSQL/BDProjetoRepositorioEF/UsuarioRepositorioEF.cs
@@ -0,0 +1,33 @@
+﻿using BDProjetoDominio;
+using BDProjetoDominio.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDProjetoRepositorioEF
+{
+    public class UsuarioRepositorioEF : IRepositorio<Usuario>
+    {
+        void IRepositorio<Usuario>.Excluir(Usuario entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        Usuario IRepositorio<Usuario>.ListarPorId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Usuario> IRepositorio<Usuario>.ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepositorio<Usuario>.Salvar(Usuario entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
